011527c016fc40c4f8c5c13d5c0b31c78cbb8bb1
Max <max30012540@hotmail.com>
2019-03-14 16:31:55 +0700

    create App component

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 /* Test edit in VS Code on MACos */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';'
+import App from './components/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
