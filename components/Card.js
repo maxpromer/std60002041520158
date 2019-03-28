f9e52639aba24d5304a961beb32cfdda8763e231
Max <max30012540@hotmail.com>
2019-03-28 16:37:14 +0700

    เพิ่มรูปใน Card

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Button } from 'react-native';
 import CardHeader from './CardHeader';
+import FitImage from 'react-native-fit-image';
 
 export default class Card extends React.Component {
     render() {
@@ -12,7 +13,26 @@ export default class Card extends React.Component {
                 marginBottom: 30,
                 elevation: 4
             }}>
-                <CardHeader />
+                <CardHeader 
+                    img={"https://pbs.twimg.com/media/C-roeGjU0AA2f90.jpg"} 
+                    title="เต & นิว" 
+                    des="#เตนิว #โพก้า #ชาวบ้าน" />
+                
+                {/* Image */}
+                <FitImage
+                    resizeMode="contain"
+                    source={{ uri: "https://pbs.twimg.com/media/DyJ6EDFU8AIwfQK.jpg:large" }} />
+                
+                <View style={{
+                    alignItems: "center",
+                    padding: 20,
+                }}>
+                    <View style={{ width: 100 }}>
+                        <Button
+                            title="Buy now"
+                            color="#841584" />
+                    </View>
+                </View>
             </View>
         );
     }
